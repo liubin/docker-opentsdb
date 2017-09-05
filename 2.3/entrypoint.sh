3b3a5538043393b54243c4e6f121afca998e08a2
bin liu <liubin0329@gmail.com>
2017-09-05 22:18:43 +0800

    add --staticroot=/opt/opentsdb/build/staticroot to startup

--- 2.3/entrypoint.sh
+++ 2.3/entrypoint.sh
@@ -15,19 +15,18 @@ if [ -n "$TABLE_PREFIX" ]; then
     table_prefix="${TABLE_PREFIX}-"
 fi
 
+# zookeeper.znode.parent in hbase config file.
+zk_basedir=${ZK_BASEDIR:-"/hbase"}
+
 echo table_prefix=$table_prefix
 echo max_tags=$max_tags
 echo ZK_QUORUM=$ZK_QUORUM
+echo zk_basedir=$zk_basedir
 
-sed -i "s|{{table_prefix}}|$table_prefix|g" /opentsdb/opentsdb.conf
-sed -i "s|{{zk_quorum}}|$ZK_QUORUM|g" /opentsdb/opentsdb.conf
-sed -i "s|{{max_tags}}|$max_tags|g" /opentsdb/opentsdb.conf
+sed -i "s|{{zk_basedir}}|$zk_basedir|g" /opt/opentsdb/opentsdb.conf
+sed -i "s|{{table_prefix}}|$table_prefix|g" /opt/opentsdb/opentsdb.conf
+sed -i "s|{{zk_quorum}}|$ZK_QUORUM|g" /opt/opentsdb/opentsdb.conf
+sed -i "s|{{max_tags}}|$max_tags|g" /opt/opentsdb/opentsdb.conf
 
-# [ -d /var/log/opentsdb ] || mkdir -p /var/log/opentsdb
-[ -f /var/log/opentsdb/opentsdb.log ] || touch /var/log/opentsdb/opentsdb.log
-
-# /usr/share/opentsdb/bin/tsdb tsd --auto-metric &
-
-# tail -f /var/log/opentsdb/opentsdb.log
-
-/opt/opentsdb/bin/tsdb tsd --auto-metric
+# start opentsdb
+/opt/opentsdb/build/tsdb tsd --auto-metric --staticroot=/opt/opentsdb/build/staticroot
